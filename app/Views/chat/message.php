abd2a2486f9bdbe895ce4d7b50dfe67eba88efa2
winarno12 <narnowin0704@gmail.com>
2022-04-29 07:58:24 +0700

    css

--- app/Views/chat/message.php
+++ app/Views/chat/message.php
@@ -1,4 +1,6 @@
-
+<?= $this->extend('layout/templates'); ?>
+<?= $this->section('content'); ?>
+<link rel="stylesheet" href="/css/chat.css">
 <div class="container">
     <div class="row clearfix">
         <div class="col-lg-12">
@@ -6,7 +8,7 @@
             <div class="chat">
                 <div class="chat-header clearfix">
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-6">
                             <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
                                 <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="avatar">
                             </a>
@@ -15,7 +17,7 @@
                                 <small>Last seen: 2 hours ago</small>
                             </div>
                         </div>
-                        <div class="col-lg-6 hidden-sm text-right">
+                        <div class="col-6 hidden-sm text-end">
                             <a href="javascript:void(0);" class="btn btn-outline-secondary"><i class="fa fa-camera"></i></a>
                             <a href="javascript:void(0);" class="btn btn-outline-primary"><i class="fa fa-image"></i></a>
                             <a href="javascript:void(0);" class="btn btn-outline-info"><i class="fa fa-cogs"></i></a>
@@ -49,7 +51,7 @@
                 <div class="chat-message clearfix">
                     <div class="input-group mb-0">
                         <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fa fa-send"></i></span>
+                            <span class="input-group-text"><i class="fa fa-send fa-2x"></i></span>
                         </div>
                         <input type="text" class="form-control" placeholder="Enter text here...">                                    
                     </div>
@@ -59,3 +61,4 @@
         </div>
     </div>
 </div>
+<?= $this->endSection(); ?>
